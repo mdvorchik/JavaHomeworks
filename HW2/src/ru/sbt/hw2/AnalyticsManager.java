685c72e46f75a658d92f25c1d1b014423a9dc26c
Maxim Dvorchik <maxim.dvorchik@yandex.ru>
2020-10-02 18:45:27 +0300

    Update AnalyticsManager.java

--- HW2/src/ru/sbt/hw2/AnalyticsManager.java
+++ HW2/src/ru/sbt/hw2/AnalyticsManager.java
@@ -1,5 +1,9 @@
 package ru.sbt.hw2;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnalyticsManager {
     private final TransactionManager transactionManager;
 
@@ -8,7 +12,27 @@ public class AnalyticsManager {
     }
 
     public Account mostFrequentBeneficiaryOfAccount(Account account) {
-        // write your code here
+        Integer max = 0;
+        Account mostFrequentBeneficiaryOfAccount = null;
+        Map<Account, Integer> mapBeneficiaryToFrequency = new HashMap<>();
+        Collection<Transaction> transactionCollection = transactionManager.findAllTransactionsByAccount(account);
+        for (Transaction transaction: transactionCollection) {
+            if (mapBeneficiaryToFrequency.containsKey(transaction.getBeneficiary())){
+                Integer tempInteger = mapBeneficiaryToFrequency.get(transaction.getBeneficiary()) + 1;
+                mapBeneficiaryToFrequency.put(transaction.getBeneficiary(), tempInteger);
+                if (max < tempInteger) {
+                    max = tempInteger;
+                    mostFrequentBeneficiaryOfAccount = transaction.getBeneficiary();
+                }
+            } else {
+                mapBeneficiaryToFrequency.put(transaction.getBeneficiary(), 1);
+                if (max < 1) {
+                    max = 1;
+                    mostFrequentBeneficiaryOfAccount = transaction.getBeneficiary();
+                }
+            }
+        }
+        return mostFrequentBeneficiaryOfAccount;
     }
 
     public Collection<Transaction> topTenExpensivePurchases(Account account) {
