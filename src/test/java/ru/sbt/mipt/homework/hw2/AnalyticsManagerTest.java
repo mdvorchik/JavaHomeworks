c31547b0d95cf1ca2e4b4b8c1570d6881fe96764
Maxim Dvorchik <maxim.dvorchik@yandex.ru>
2021-05-31 17:08:18 +0300

    refactor AnalyticsManagerTest

--- src/test/java/ru/sbt/mipt/homework/hw2/AnalyticsManagerTest.java
+++ src/test/java/ru/sbt/mipt/homework/hw2/AnalyticsManagerTest.java
@@ -1,7 +1,5 @@
 package ru.sbt.mipt.homework.hw2;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,59 +7,13 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class AnalyticsManagerTest {
 
-    private TransactionManager transactionManager;
-    private AnalyticsManager analyticsManager;
-    private Transaction transaction;
-    private ArrayList<Transaction> expectedTopTenExpensivePurchases;
-    private Account account1;
-    private Account account2;
-    private Account account3;
-    private Account account4;
     private final Comparator<Transaction> transactionComparatorByAmount =
             (Transaction t1, Transaction t2) -> Double.compare(t2.getAmount(), t1.getAmount());
-
-    @Before
-    public void setUp() {
-        HashMap<Account, List<Transaction>> hashMap = new HashMap<>();
-        ArrayList<Transaction> transactions = new ArrayList<>();
-        transactionManager = new TransactionManager(hashMap, transactions);
-        analyticsManager = new AnalyticsManager(transactionManager);
-        expectedTopTenExpensivePurchases = new ArrayList<>();
-        account1 = new Account(1, transactionManager);
-        account2 = new Account(2, transactionManager);
-        account3 = new Account(3, transactionManager);
-        account4 = new Account(4, transactionManager);
-        transaction = transactionManager.createTransaction(1000, account1, account3);
-        transaction = transactionManager.createTransaction(15000, account1, account3);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(200, account1, account4);
-        transaction = transactionManager.createTransaction(20000, account1, account2);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(1700, account1, account3);
-        transaction = transactionManager.createTransaction(30000, account1, account4);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(30000, account1, account4);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(10000, account1, account4);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(9000, account1, account2);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(9000, account1, account3);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(7000, account1, account4);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(100000, account1, account2);
-        expectedTopTenExpensivePurchases.add(transaction);
-        transaction = transactionManager.createTransaction(1050, account1, account3);
-        transaction = transactionManager.createTransaction(1300, account1, account4);
-        transaction = transactionManager.createTransaction(1700, account1, account3);
-        transaction = transactionManager.createTransaction(4000, account1, account2);
-        transaction = transactionManager.createTransaction(1000000, account1, account4);
-        expectedTopTenExpensivePurchases.add(transaction);
-        expectedTopTenExpensivePurchases.sort(transactionComparatorByAmount);
-    }
 
     /**
      * total count beneficiary by account1:
@@ -71,18 +23,80 @@ public class AnalyticsManagerTest {
      */
     @Test
     public void mostFrequentBeneficiaryOfAccount() {
-        Assert.assertEquals(account4, analyticsManager.mostFrequentBeneficiaryOfAccount(account1));
+        //given
+        TransactionManager transactionManager = prepareTransactionManager();
+        AnalyticsManager analyticsManager = new AnalyticsManager(transactionManager);
+        List<Account> accountList = prepareAccountList(transactionManager);
+        fillAccountsByTrashTransactions(transactionManager, accountList);
+        prepareTopTenExpensivePurchases(transactionManager, accountList);
+        //verify
+        assertEquals(accountList.get(3), analyticsManager.mostFrequentBeneficiaryOfAccount(accountList.get(0)));
     }
 
     @Test
     public void transactionComparatorByAmountTest() {
-        Assert.assertEquals(expectedTopTenExpensivePurchases.get(0).getAmount(), 1000000, 0.0001);
-        Assert.assertEquals(expectedTopTenExpensivePurchases.get(9).getAmount(), 7000, 0.0001);
+        //given
+        TransactionManager transactionManager = prepareTransactionManager();
+        List<Account> accountList = prepareAccountList(transactionManager);
+        fillAccountsByTrashTransactions(transactionManager, accountList);
+        List<Transaction> transactionList = prepareTopTenExpensivePurchases(transactionManager, accountList);
+        //verify
+        assertEquals(transactionList.get(0).getAmount(), 1000000, 0.0001);
+        assertEquals(transactionList.get(9).getAmount(), 7000, 0.0001);
     }
 
     @Test
     public void topTenExpensivePurchases() {
-        Assert.assertArrayEquals(expectedTopTenExpensivePurchases.toArray(), analyticsManager.topTenExpensivePurchases(account1).toArray());
+        //given
+        TransactionManager transactionManager = prepareTransactionManager();
+        AnalyticsManager analyticsManager = new AnalyticsManager(transactionManager);
+        List<Account> accountList = prepareAccountList(transactionManager);
+        fillAccountsByTrashTransactions(transactionManager, accountList);
+        List<Transaction> transactionList = prepareTopTenExpensivePurchases(transactionManager, accountList);
+        //verify
+        assertArrayEquals(transactionList.toArray(), analyticsManager.topTenExpensivePurchases(accountList.get(0)).toArray());
+    }
 
+    private TransactionManager prepareTransactionManager() {
+        return new TransactionManager(new HashMap<>(), new ArrayList<>());
+    }
+
+    private List<Account> prepareAccountList(TransactionManager transactionManager) {
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(1, transactionManager));
+        accounts.add(new Account(2, transactionManager));
+        accounts.add(new Account(3, transactionManager));
+        accounts.add(new Account(4, transactionManager));
+        return accounts;
+    }
+
+    private void fillAccountsByTrashTransactions(TransactionManager transactionManager, List<Account> accounts) {
+        if (accounts.size() > 3) {
+            transactionManager.createTransaction(1000, accounts.get(0), accounts.get(2));
+            transactionManager.createTransaction(200, accounts.get(0), accounts.get(3));
+            transactionManager.createTransaction(1700, accounts.get(0), accounts.get(2));
+            transactionManager.createTransaction(1050, accounts.get(0), accounts.get(2));
+            transactionManager.createTransaction(1300, accounts.get(0), accounts.get(3));
+            transactionManager.createTransaction(1700, accounts.get(0), accounts.get(2));
+            transactionManager.createTransaction(4000, accounts.get(0), accounts.get(1));
+        }
+    }
+
+    private List<Transaction> prepareTopTenExpensivePurchases(TransactionManager transactionManager, List<Account> accounts) {
+        List<Transaction> transactions = new ArrayList<>();
+        if (accounts.size() > 3) {
+            transactions.add(transactionManager.createTransaction(15000, accounts.get(0), accounts.get(2)));
+            transactions.add(transactionManager.createTransaction(20000, accounts.get(0), accounts.get(1)));
+            transactions.add(transactionManager.createTransaction(30000, accounts.get(0), accounts.get(3)));
+            transactions.add(transactionManager.createTransaction(30000, accounts.get(0), accounts.get(3)));
+            transactions.add(transactionManager.createTransaction(10000, accounts.get(0), accounts.get(3)));
+            transactions.add(transactionManager.createTransaction(9000, accounts.get(0), accounts.get(1)));
+            transactions.add(transactionManager.createTransaction(9000, accounts.get(0), accounts.get(2)));
+            transactions.add(transactionManager.createTransaction(7000, accounts.get(0), accounts.get(3)));
+            transactions.add(transactionManager.createTransaction(100000, accounts.get(0), accounts.get(1)));
+            transactions.add(transactionManager.createTransaction(1000000, accounts.get(0), accounts.get(3)));
+            transactions.sort(transactionComparatorByAmount);
+        }
+        return transactions;
     }
 }
